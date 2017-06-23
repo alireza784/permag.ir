c8d9d1f885a5b0d0f5b521026e91f1ba56b1aedd
alireza784 <githubalireza@yahoo.com>
2017-06-23 15:24:52 +0430

    Update tdcli.lua

--- tg/tdcli.lua
+++ tg/tdcli.lua
@@ -3,17 +3,14 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
    MA 02110-1301, USA.
-
 ]]--
 
 -- Vector example form is like this: {[0] = v} or {v1, v2, v3, [0] = v}
@@ -2704,5 +2701,7 @@ M.sendForwarded = sendForwarded
 return M
 
 -- کد های پایین در ربات نشان داده نمیشوند
--- @TeleJetTeam
-
+-- http://permag.ir
+-- @permag_ir
+-- @permag_bots
+-- @permag
